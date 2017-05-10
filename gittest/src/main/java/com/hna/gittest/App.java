de14fa9fb4b5d7be6a2d017ec8a12486abe91cfb
winy <jiant.wu@hnair.com>
2017-05-10 22:10:20 +0800

    1-2

--- gittest/src/main/java/com/hna/gittest/App.java
+++ gittest/src/main/java/com/hna/gittest/App.java
@@ -7,6 +7,7 @@ package com.hna.gittest;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("WJT");
 
     }
 }
